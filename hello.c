d31326deeae9acada8f2c3a08690fe966745c84c
BazookaGoBoom <BazookaGoBoom@users.noreply.github.com>
2020-10-06 23:29:09 +0000

    automated commit by check50 [check50=True]

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("What is your name?");
+    printf("hello, %s\n", name);
+}
+check50 cs50/problems/2020/x/hello;
